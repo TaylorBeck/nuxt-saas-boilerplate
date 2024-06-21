3f3c3863994ecb7bf3ab5d6c3b5133eee8047c9e
Taylor Beck <beck.taylorg@gmail.com>
2024-06-21 13:59:24 -0700

    Set up nuxt project

---
+++ nuxt.config.ts
@@ -0,0 +1,12 @@
+export default defineNuxtConfig({
+  modules: ['@nuxtjs/tailwindcss'],
+  app: {
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+      link: [
+        { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap' }
+      ]
+    }
+  }
+})
